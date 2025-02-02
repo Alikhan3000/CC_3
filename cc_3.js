914ca2ca5771ee5e4a92b7796cf1c77b4c89a629
Alikhan <fixator2004@gmail.com>
2025-02-02 14:46:21 -0500

    Task 1 - Product Price Management.

---
+++ cc_3.js
@@ -0,0 +1,8 @@
+//Task 1: Product Price Management Scenario: Online Store You are managing a product pricing system.
+
+let prices = [100, 200, 300, 400, 500]; //declared an array with 5 values
+
+prices.push(600); //new value 600 added to the array 
+prices.shift();  //removed the first element from the array
+
+console.log(prices); 
